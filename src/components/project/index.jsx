e5629f0c334648e10a79dc39d7040d376721862c
nikorekhviashvili <54753313+nikorekhviashvili@users.noreply.github.com>
2025-04-08 19:56:09 +0200

    mobile fixes

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -1,16 +1,36 @@
 'use client';
-import React from 'react'
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import styles from './style.module.css';
 
 export default function index({index, title, description, link, onHover, headingLevel = 'h2'}) {
     const Heading = headingLevel;
+    const router = useRouter();
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        if (isActive) {
+            router.push(link);
+        } else {
+            onHover(true, index);
+            setIsActive(true);
+        }
+    };
+
+    const handleMouseLeave = () => {
+        onHover(false, index);
+        setIsActive(false);
+    };
     
     return (
-        <a href={link} className={styles.project} 
+        <div 
+           className={styles.project} 
            onMouseEnter={() => onHover(true, index)} 
-           onMouseLeave={() => onHover(false, index)}>
+           onMouseLeave={handleMouseLeave}
+           onClick={handleClick}
+        >
             <Heading className={styles.projectTitle}>{title}</Heading>
             <p>{description}</p>
-        </a>
+        </div>
     )
 }
